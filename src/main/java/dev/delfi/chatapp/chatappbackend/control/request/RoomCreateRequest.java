e4261f23c2527760ab6f71af94b9a8df91c5790e
Delfi-CH <laspitzer@gmx.ch>
2025-12-10 17:05:20 +0100

    fix json for room create request

--- src/main/java/dev/delfi/chatapp/chatappbackend/control/request/RoomCreateRequest.java
+++ src/main/java/dev/delfi/chatapp/chatappbackend/control/request/RoomCreateRequest.java
@@ -1,5 +1,6 @@
 package dev.delfi.chatapp.chatappbackend.control.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +8,17 @@ import lombok.Setter;
 @Setter
 public class RoomCreateRequest {
     private String name;
+
+    @JsonProperty("is_public")
     private boolean isPublic;
+
+    @JsonProperty("is_directmessage")
     private boolean isDirectMessage;
+
+    @JsonProperty("max_users")
     private Long maxUsers = 10L;
+
+    @JsonProperty("owner_user_id")
     private Long ownerUserId;
 }
+
