c02191e0e5fbd714b6327ec2f3f32760b5613823
Delfi-CH <laspitzer@gmx.ch>
2026-01-07 15:48:08 +0100

    fix PasswordUpdateRequest.java

--- src/main/java/dev/delfi/chatapp/chatappbackend/control/request/PasswordUpdateRequest.java
+++ src/main/java/dev/delfi/chatapp/chatappbackend/control/request/PasswordUpdateRequest.java
@@ -1,6 +1,11 @@
 package dev.delfi.chatapp.chatappbackend.control.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class PasswordUpdateRequest {
+
+    @JsonProperty("old_password")
     public String oldPassword;
+    @JsonProperty("new_password")
     public String newPassword;
 }
